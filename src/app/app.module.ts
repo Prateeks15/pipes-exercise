63ed76acaa8084abaee70473dde378f01cf192ed
Prateeks15 <prateekchadha15@gmail.com>
2021-04-08 22:10:53 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReversePipe} from './reverse.pipe';
+import { CountVowel } from './count.pipe';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppComponent } from './app.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReversePipe,
+    CountVowel
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
